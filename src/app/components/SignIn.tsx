f78740270b8daa0ae7eac07a821bb0534aaf0fb0
Rimas Kalvelis <rimas.kalvelis@cabinetoffice.gov.uk>
2024-07-02 15:30:26 +0100

    getting rid of google button

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -3,5 +3,5 @@
 import { signIn } from "next-auth/react";
 
 export default function SignIn({}) {
-  return <button onClick={() => signIn()}>Sign in</button>;
+  return <button onClick={() => signIn('google')}>Sign in</button>;
 }
